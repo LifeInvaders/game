c737f202ada4727a170c13677588454d3775276d
Harrys Kedjnane <harrys.kedjane@epita.fr>
2020-12-24 04:53:03 +0100

    Character creation BETA
    Character customization menu and instanciation -- still in development

---
+++ Assets/Scripts/Player/CharacterCreation.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using static PlayerDatabase;
+
+namespace Player
+{
+    public class CharacterCreation : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ChangeSkin();
+        }
+
+        private static string _charID()
+        {
+        char[] charArray = {Rank, Gender, Variant, SkinColor};
+        return new string(charArray);
+        }
+        private void ChangeSkin()
+        {
+            string searchName = _charID();
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.name == searchName)
+                    child.gameObject.SetActive(true);
+                else if (child.gameObject.activeSelf)
+                    child.gameObject.SetActive(false);
+            }
+        }
+
+        public void ChangeData(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    Rank = (Rank + 1 > '2') ? '0' : (char) (Rank + 1);
+                    break;
+                case 1:
+                    Gender = (Gender == 'F') ? 'M' : 'F';
+                    break;
+                case 2:
+                    Variant = (Variant + 1 > '4') ? '1' : (char) (Variant + 1);
+                    break;
+                case 3:
+                    SkinColor = (SkinColor + 1 > 'C') ? 'A' : (char) (SkinColor + 1);
+                    break;
+                default:
+                    Debug.Log("wrong value");
+                    break;
+            }
+            ChangeSkin();
+        }
+
+    }   
+}
---
+++ Assets/Scripts/Player/PlayerDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public class PlayerDatabase : MonoBehaviour
+{
+    #region Stats
+
+    public static int TargetKills = 0;
+    public static int Points = 0;
+    public static int Deaths = 0;
+    public static int Games = 0;
+
+    #endregion      
+    
+   #region PlayerCharacter
+
+   public static char Rank = '0';
+   public static char Gender = 'M';
+   public static char Variant = '1';
+   public static char SkinColor = 'A';
+
+   #endregion
+    
+    #region Progress
+    
+    public static int Level = 0;
+    public static int Exp = 0;
+
+    #endregion
+    
+    
+}
