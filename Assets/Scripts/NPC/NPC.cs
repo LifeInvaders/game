1e08ff47de1c61ef0a81e0de52fe17acba322a47
Renaud-Dov <dov.devers@gmail.com>
2020-12-28 18:36:40 +0100

    add Doors + Patrol and general IA

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    private Rigidbody rig;
+    private Animator anim;
+    private NavMeshAgent agent;
+    public Transform goal;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        rig = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        
+        anim.SetBool("walk",true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        agent.destination = goal.position;
+
+        // agent.destination = new Vector3(transform.position.x + Random.Range(-5, 5), 0,
+        //     transform.position.x + Random.Range(-20, 20));
+    }
+    
+
+}
---
+++ Assets/Scripts/Objects/Doors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    private Transform left_door;
+
+    private Transform right_door;
+
+    private bool opened = true;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        left_door = transform.Find("left door");
+        right_door = transform.Find("right door");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (opened && other.gameObject.CompareTag("Player") && other.gameObject.GetComponent<PlayerControler>().Running())
+        {
+            opened = false;
+            Debug.Log($"{left_door.eulerAngles} {right_door.eulerAngles}");
+            left_door.eulerAngles -= Vector3.up * -90;
+            right_door.eulerAngles -= Vector3.up * 90;
+            StartCoroutine(ExampleCoroutine());
+        }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        
+        yield return new WaitForSeconds(5);
+        
+        opened = true;
+        Debug.Log($"{left_door.eulerAngles} {right_door.eulerAngles}");
+        left_door.eulerAngles += Vector3.up * (-90);
+        right_door.eulerAngles += Vector3.up * 90;
+        Debug.Log($"{left_door.eulerAngles} {right_door.eulerAngles}");
+    }
+}
