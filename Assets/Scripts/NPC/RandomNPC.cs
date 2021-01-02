e10b6a76b5b36b37efc4d8aa7c066554218c8e9f
Renaud-Dov <dov.devers@gmail.com>
2021-01-02 16:00:38 +0100

    NPC prefabs

---
+++ Assets/Scripts/NPC/RandomNPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RandomNPC : MonoBehaviour
+{
+    private SkinnedMeshRenderer renderer;
+
+    public Mesh[] meshes;
+
+    public Material[] materials;
+    // private Mesh meshes[];
+    // public Mesh mesh;
+    //
+    // public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SkinnedMeshRenderer>();
+        renderer.sharedMesh = meshes[Random.Range(0,meshes.Length-1)];
+        renderer.sharedMaterial = materials[Random.Range(0, materials.Length - 1)];
+    }
+
+
+}
