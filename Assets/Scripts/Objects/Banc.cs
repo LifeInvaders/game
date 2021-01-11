1ccb8f4b6e4e73e7ac4f42961e8a27ca8625defe
Renaud-Dov <dov.devers@gmail.com>
2021-01-11 19:20:48 +0100

    player bug fix

--- Assets/Scripts/Objects/Banc.cs
+++ Assets/Scripts/Objects/Banc.cs
@@ -26,6 +26,16 @@ public class Banc : MonoBehaviour
         {
             Debug.Log("new player");
             // _playerList.Add(other.gameObject.);
+            
+            /*/*Set Sitting animation#1#
+            anim.SetBool("sitting",true);
+            capsule.center = new Vector3(capsule.center.x,0.95f,capsule.center.z);
+            capsule.height = 1.1f;
+            canRotate = false;
+            transform.eulerAngles = col.gameObject.transform.eulerAngles + 180 * Vector3.up;
+            transform.position = col.gameObject.transform.position;
+            // transform.rotation.x += 180;
+            // canMove = false;*/
         }
     }
 
@@ -34,6 +44,12 @@ public class Banc : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("remove player");
+            // /*Go back to normal animation*/
+            // canRotate = true;
+            // // canMove = true;
+            // anim.SetBool("sitting",false);
+            // capsule.center = new Vector3(capsule.center.x,0.9f,capsule.center.z);
+            // capsule.height = 1.97f;
         }
     }
 }
