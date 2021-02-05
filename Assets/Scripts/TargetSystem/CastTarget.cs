9a0325bb5216e3beab3502cbb26a1ffafb5ba03c
Renaud-Dov <dov.devers@gmail.com>
2021-02-05 17:09:22 +0100

    add selected target system

--- Assets/Scripts/TargetSystem/CastTarget.cs
+++ Assets/Scripts/TargetSystem/CastTarget.cs
@@ -9,49 +9,59 @@ namespace TargetSystem
     {
         public RaycastHit raycastHit;
         [SerializeField] private Camera _camera;
-        private Outline _outlinetarget;
 
         private Outline _outlinecam;
         private GameObject _target;
 
         private bool _isOutlinecamNotNull;
+        private SelectedTarget _selectedTarget;
+
+        private void Start()
+        {
+            _selectedTarget = GetComponent<SelectedTarget>();
+        }
+
+        private void Outlining(Outline playerOutline)
+        {
+            if (_target != null && _target.name != raycastHit.transform.gameObject.name)
+            {
+                // Not the same player
+                _outlinecam.enabled = false;
+                _target = raycastHit.transform.gameObject;
+                _outlinecam = playerOutline;
+                _outlinecam.enabled = true;
+            }
+            else
+            {
+                _outlinecam = playerOutline;
+                _target = raycastHit.transform.gameObject;
+                _outlinecam.enabled = true;
+            }
+        }
 
         private void Update()
         {
-
             if (Input.GetButton("Fire2"))
             {
                 if (Physics.Raycast(_camera.transform.position, _camera.transform.TransformDirection(Vector3.forward),
-                    out raycastHit,30f) && (raycastHit.transform.gameObject.CompareTag("Player") ||
-                                        raycastHit.transform.gameObject.CompareTag("NPC")))// && Vector3.Distance(raycastHit.transform.position,transform.position) < 12)
+                        out raycastHit, 30f) && (raycastHit.transform.gameObject.CompareTag("Player") ||
+                                                 raycastHit.transform.gameObject.CompareTag("NPC"))
+                ) // && Vector3.Distance(raycastHit.transform.position,transform.position) < 12)
                 {
-                    Outline playerOutline = raycastHit.transform.Find("Character").GetComponent<Outline>();
-
-
-                    if (_target != null && _target.name != raycastHit.transform.gameObject.name)
+                    if (!_selectedTarget.IsTarget())
                     {
-                        // Not the same player
-                        _outlinecam.enabled = false;
-                        _target = raycastHit.transform.gameObject;
-                        _outlinecam = playerOutline;
-                        _outlinecam.enabled = true;
+                        Outlining(raycastHit.transform.Find("Character").GetComponent<Outline>());
                     }
-                    else
+                    if (Input.GetButtonDown("Fire1"))
                     {
-                        _outlinecam = playerOutline;
-                        _target = raycastHit.transform.gameObject;
-                        _outlinecam.enabled = true;
+                        _selectedTarget.UpdateSelectedTarget(_target, _outlinecam);
                     }
                 }
-                else if (_target != null)
-                {
+                else if (_target != null && !_selectedTarget.IsSelectedTarget(_target))
                     RemoveCamTarget();
-                }
             }
-            else if (_target != null)
-            {
+            else if (_target != null && !_selectedTarget.IsSelectedTarget(_target))
                 RemoveCamTarget();
-            }
         }
 
         private void RemoveCamTarget()
---
+++ Assets/Scripts/TargetSystem/SelectedTarget.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace TargetSystem
+{
+    public class SelectedTarget : MonoBehaviour
+    {
+        private GameObject _selectedTarget;
+        private Outline _outlineTarget;
+        private bool _isselectedtarget = false;
+        private RaycastHit _raycastHit;
+
+        [SerializeField] private Camera camera;    
+        // Start is called before the first frame update
+
+
+        // Update is called once per frame
+        private void ResetOutline()
+        {
+
+            if (_selectedTarget != null)
+            {
+                _outlineTarget.OutlineColor = Color.white;
+                _outlineTarget.enabled = false;
+                _outlineTarget = null;
+            }
+        }
+
+        public bool IsTarget()
+        {
+            return _isselectedtarget;
+        }
+
+        public GameObject GetTarget()
+        {
+            return _selectedTarget;
+        }
+
+        public bool IsSelectedTarget(GameObject target)
+        {
+            
+            return _selectedTarget != null && target.name == _selectedTarget.name;
+        }
+
+        public void UpdateSelectedTarget(GameObject target,Outline outline)
+        {
+            ResetOutline();
+            if (IsSelectedTarget(target))
+            {
+                _selectedTarget = null;
+                _isselectedtarget = false;
+            }
+            else
+            {
+                _selectedTarget = target;
+                _outlineTarget = outline;
+                _outlineTarget.OutlineColor = Color.yellow;
+                _isselectedtarget = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (IsTarget() && Vector3.Distance(_selectedTarget.transform.position,transform.position) > 30)
+            {
+                UpdateSelectedTarget(_selectedTarget, _outlineTarget);
+            }
+        }
+    }
+}
