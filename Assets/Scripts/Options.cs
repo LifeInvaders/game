f186079ce081235559fdc8e2d10c03e18a9be860
Harrys Kedjnane <harrys.kedjane@epita.fr>
2021-02-23 22:35:51 +0100

    Modified menu further
    Need to add some settings to save file

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+
+    void Start()
+    {
+        float volume;
+        soundSystem.GetFloat("volume",out volume);
+        volumeSlider.value = volume;
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        fullscreenToggle.isOn = Screen.fullScreen;
+        soundSystem.FindMatchingGroups("Master");
+    }
+    [SerializeField] private AudioMixer soundSystem;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullscreenToggle;
+
+
+
+    public void Fullscreen(bool check) => Screen.fullScreen = check;
+
+    public void Quality(int index) => QualitySettings.SetQualityLevel(index);
+
+    public void SetVolume(float vol) => soundSystem.SetFloat("volume", vol);
+}
