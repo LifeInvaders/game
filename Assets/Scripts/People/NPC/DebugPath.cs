63619befa8add1018693b0cf4c9a82d641f0ae59
Renaud-Dov <dov.devers@gmail.com>
2021-04-09 19:07:43 +0200

    update IA path + npc zone

---
+++ Assets/Scripts/People/NPC/DebugPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DebugPath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 target;
+    private NavMeshPath path;
+    private float elapsed = 0.0f;
+    public Color color = Color.red;
+    void Start()
+    {
+        path = new NavMeshPath();
+        elapsed = 0.0f;
+    }
+
+    void Update()
+    {
+        // Update the way to the goal every second.
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            elapsed -= 1.0f;
+            NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+        }
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], color);
+    }
+}
