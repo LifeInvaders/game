09a2d660bef80f442c927b3ebfc024dcfebf6c68
Harrys Kedjnane <harrys.kedjane@epita.fr>
2021-04-22 15:31:59 +0200

    Build Pipeline

--- Assets/Scripts/Utils/Editor/BuildPipeline.cs
+++ Assets/Scripts/Utils/Editor/BuildPipeline.cs
@@ -1,43 +1,15 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
 using System.IO;
-using Unity.Collections.LowLevel.Unsafe;
+using System.IO.Compression;
+using Debug = UnityEngine.Debug;
 
 
-class BuildSettings : ScriptableObject
-{
-    public const string CustomSettingsPath = "Assets/Editor/CustomSettings.asset";
-    
-    public string buildsPath;
-    public string lastName;
-
-    public bool windowsBuild;
-    public bool linuxBuild;
-    public bool macBuild;
-    
-    public int instances;
-
-    internal static BuildSettings GetOrCreateSettings()
-    {
-        var settings = AssetDatabase.LoadAssetAtPath<BuildSettings>(CustomSettingsPath);
-        if (settings == null)
-        {
-            settings = CreateInstance<BuildSettings>();
-            settings.buildsPath = Directory.GetCurrentDirectory();
-            settings.windowsBuild = false;
-            settings.linuxBuild = false;
-            settings.macBuild = false;
-            settings.instances = 1;
-            settings.lastName = "";
-            AssetDatabase.CreateAsset(settings, CustomSettingsPath);
-            AssetDatabase.SaveAssets();
-        }
-        return settings;
-    }
-}
 public class BuildPipeline : EditorWindow
 {
-    private BuildSettings _buildSettings;
+    private BuildPipelineSO _buildSettings;
     
     [MenuItem("Tools/Build Pipeline")]
     public static void OpenBuildWindow()
@@ -54,7 +26,7 @@ public class BuildPipeline : EditorWindow
 
     void OnGUI()
     {
-		_buildSettings = BuildSettings.GetOrCreateSettings();
+		_buildSettings = BuildPipelineSO.GetOrCreateSettings();
         GUILayout.Label ("Build Settings", EditorStyles.boldLabel);
         var buildPath = GUILayout.Button(_buildSettings.buildsPath);
         var versionName = EditorGUILayout.TextField("Build name",_buildSettings.lastName);
@@ -69,16 +41,71 @@ public class BuildPipeline : EditorWindow
         _buildSettings.linuxBuild = linuxToggle;
         _buildSettings.macBuild = macToggle;
         _buildSettings.instances = instancesSlider;
-        if (build) Build();
+        if (build) InitiateBuild();
     }
 
-    void Build()
+    void InitiateBuild()
     {
-        Directory.SetCurrentDirectory(_buildSettings.buildsPath);
-        Directory.CreateDirectory(_buildSettings.lastName);
-        Directory.SetCurrentDirectory(_buildSettings.lastName);
-        if (_buildSettings.windowsBuild) Directory.CreateDirectory(Windows);
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        var path = Path.Combine(_buildSettings.buildsPath, _buildSettings.lastName);
+        Directory.CreateDirectory(path);
+        if (_buildSettings.windowsBuild) Build(BuildTarget.StandaloneWindows64);
+        if (_buildSettings.linuxBuild) Build(BuildTarget.StandaloneLinux64);
+        if (_buildSettings.macBuild) Build(BuildTarget.StandaloneOSX);
+        stopWatch.Stop();
+        Debug.Log("Building sequence finished after " + stopWatch.ElapsedMilliseconds + " ms.");
 
+
+    }
+
+    void Build(BuildTarget buildTarget)
+    {
+        var path = Path.Combine(_buildSettings.buildsPath, _buildSettings.lastName, buildTarget.ToString());
+        Directory.CreateDirectory(path);
+        int i = 1;
+        while (Directory.Exists(Path.Combine(path, "v" + i))) {i++;}
+        string version = "v" + i;
+        path = Path.Combine(path, version);
+        Directory.CreateDirectory(path);
+        DirectoryInfo parentDir = new DirectoryInfo(path);
+        path = Path.Combine(path, "Instance 1");
+        Directory.CreateDirectory(path);
+        DirectoryInfo instanceDir = new DirectoryInfo(path);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes),
+            target = buildTarget,
+            locationPathName = Path.Combine(path, "Panic_At_Tortuga.exe"),
+            options = BuildOptions.None
+        };
+        UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+        ZipFile.CreateFromDirectory(path, Path.Combine(parentDir.FullName, buildTarget + "_" + version + ".zip"));
+        for (int instance = 2; instance <= _buildSettings.instances; instance++)
+        {
+            DirectoryInfo newInstance = new DirectoryInfo(Path.Combine(parentDir.FullName, "Instance " + instance));
+            CopyAll(instanceDir, newInstance);
+        }
+
+    }
+    
+    public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+
+        // Copy each file into the new directory.
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+
+        // Copy each subdirectory using recursion.
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+        {
+            DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
     }
 
 }
