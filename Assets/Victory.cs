8beb9ecc889175c048c0b8c2fe9532c8373d3090
Renaud-Dov <dov.devers@gmail.com>
2021-04-25 23:38:21 +0200

    add Victory Prefab system

--- Assets/Victory.cs
+++ Assets/Victory.cs
@@ -1,38 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Victory : MonoBehaviour
 {
-    [Header("Winners")] 
-    [SerializeField] private GameObject FirstWinner;
-    [SerializeField] private GameObject SecondWinner;
-    [SerializeField] private GameObject ThirdWinner;
-    
-    [Header("Losers")] 
-    [SerializeField] private GameObject FirstLoser;
-    [SerializeField] private GameObject SecondLoser;
-    [SerializeField] private GameObject ThirdLoser;
-    
-    [Header("WinnerText")]
-    [SerializeField] private TextMeshPro FirstWinnerText;
-    [SerializeField] private TextMeshPro SecondWinnerText;
-    [SerializeField] private TextMeshPro ThirdWinnerText;
-    
-    [Header("LoosersText")]
-    [SerializeField] private TextMeshPro FirstLoserText;
-    [SerializeField] private TextMeshPro SecondLoserText;
-    [SerializeField] private GameObject ThirdLoserText;
+    [Header("Winners")] [SerializeField] private GameObject[] winners;
+    [SerializeField] private TextMeshPro[] textWinners;
+
+    [Header("Losers")] [SerializeField] private GameObject[] losers;
+    [SerializeField] private TextMeshPro[] textlosers;
+
+    [SerializeField] private TextMeshPro[] countDeath;
     // Start is called before the first frame update
-    void Start()
+
+    [Header("Camera")] [SerializeField] private CinemachineVirtualCamera _camera2;
+    private int[] anims = new int[3];
+
+    public void StartAnim(int index)
     {
-        
+        Debug.Log(index);
+        winners[index].GetComponent<Animator>().SetTrigger((anims[index] + 1).ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitWinner(int index, SkinnedMeshRenderer skinnedMeshRenderer, string namePlayer, int animId)
     {
-        
+        var meshRenderer = winners[index].GetComponentInChildren<SkinnedMeshRenderer>();
+        meshRenderer.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterial = skinnedMeshRenderer.sharedMaterial;
+
+        switch (index)
+        {
+            case 0:
+                textWinners[0].text = $"1st\n{namePlayer}";
+                break;
+            case 1:
+                textWinners[1].text = $"2nd\n{namePlayer}";
+                break;
+            case 2:
+                textWinners[2].text = $"3rd\n{namePlayer}";
+                break;
+        }
+
+
+        anims[index] = animId;
+    }
+
+    public void ChangeCamera()
+    {
+        _camera2.Priority = 400;
+    }
+
+    public void InitLoser(int index, SkinnedMeshRenderer skinnedMeshRenderer, string namePlayer, int deathCount)
+    {
+        var meshRenderer = winners[index].GetComponentInChildren<SkinnedMeshRenderer>();
+        meshRenderer.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterial = skinnedMeshRenderer.sharedMaterial;
+        textlosers[index].text = namePlayer;
+
+        countDeath[index].text = $"{deathCount}\nDeath{(deathCount > 1 ? 's' : ' ')}";
+    }
+
+    public void Start()
+    {
+        // var nightMode = FindObjectOfType<NightMode>();
+        // var isDay = nightMode.IsDay();
+        // foreach (var light in GetComponentsInChildren<Light>())
+        //     light.enabled = !isDay;
+
+        string[] name = new[] {"toto", "paul", "julien", "harrys", "dov"};
+        for (int i = 0; i < 3; i++)
+        {
+            var random = new System.Random();
+            InitWinner(i, winners[i].GetComponentInChildren<SkinnedMeshRenderer>(), name[random.Next(name.Length)], i);
+            InitLoser(i, GetComponentInChildren<SkinnedMeshRenderer>(), name[random.Next(name.Length)], i + 1);
+        }
     }
 }
