56c6175b43bdc62bccd7c26847b9342309b4015a
Paul de la Porte <paul.de-la-porte-des-vaux@epita.fr>
2021-06-16 18:10:21 +0200

    ajout seconde map + prefab tyrolienne

--- Assets/Scripts/Objects/ZiplineEnter.cs
+++ Assets/Scripts/Objects/ZiplineEnter.cs
@@ -1,48 +1,87 @@
-using System;
 using System.Collections;
+using People;
 using People.Player;
 using UnityEngine;
 
 namespace Objects
 {
-    public class Zipline : MonoBehaviour
+    public class ZiplineEnter : MonoBehaviour
     {
         [SerializeField] private Transform startPos;
         [SerializeField] private Transform endPos;
+        [SerializeField] private float speed;
+        [SerializeField] private LineRenderer _lineRenderer;
         private Vector3 translateVector3;
-        private bool isMoving;
 
         public void Start()
         {
-            translateVector3 = endPos.position - startPos.position;
+            // translateVector3 = transform.parent.TransformDirection(_lineRenderer.GetPosition(1)) -  transform.parent.TransformDirection(_lineRenderer.GetPosition(0));
+            translateVector3 = endPos.transform.position - startPos.transform.position;
         }
 
         public void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.CompareTag("MyPlayer")) // si collision avec autre joueur
+            if (other.gameObject.CompareTag("MyPlayer") && other.GetComponent<PlayerEvent>().humanTask != HumanTasks.Zipline) // si collision avec autre joueur
             {
+                // change all the player's parameters
                 PlayerControler player = other.gameObject.GetComponent<PlayerControler>();
-                other.gameObject.GetComponent<Rigidbody>().useGravity = false; // on désactive la gravité du joueur
+                player.GetComponent<PlayerEvent>().humanTask = HumanTasks.Zipline;
+                var rigidbody = other.gameObject.GetComponent<Rigidbody>();
+                rigidbody.useGravity = false; // on désactive la gravité du joueur
+                rigidbody.isKinematic = true;
                 player.SetRotateBool(false);
                 player.SetMoveBool(false); // on désactive les mouvements
-                player.transform.position = startPos.position;
-                player.transform.rotation =
-                    Quaternion.Euler(0, startPos.rotation.eulerAngles.y,
-                        0); // on tourne le personnage de façon adéquate
-                isMoving = true;
+                player.CheckIfRunning();
+                player.transform.position =
+                    new Vector3(transform.position.x, transform.position.y - 0.8f, transform.position.z);
+                player.transform.rotation = transform.rotation;
+                other.GetComponent<Animator>().SetTrigger("zipline");
+
+                // spawn a sword in the player's hand
+                var sword = other.transform.Find("Root/Hips/Spine_01/Spine_02/Spine_03/Clavicle_R/Shoulder_R/Elbow_R/Hand_R/sword");
+                sword.gameObject.SetActive(true);
+                // var rotation = sword.transform.rotation;
+                // rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.y-60);
+                // sword.transform.rotation = rotation;
+
+                // start to move
+                StartCoroutine(CheckforJump(other.gameObject));
                 StartCoroutine(MovingOn(other.gameObject));
             }
         }
 
+        private IEnumerator CheckforJump(GameObject player)
+        {
+            var controler = player.GetComponent<PlayerControler>();
+            while (controler.GetAxis().y >= 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            var playerEvent = player.GetComponent<PlayerEvent>();
+            controler.enabled = true;
+            Rigidbody rigidbody = player.gameObject.GetComponent<Rigidbody>();
+            rigidbody.useGravity = true; // on réactive la gravité du joueur
+            controler.SetRotateBool(true);
+            controler.SetMoveBool(true); // on réactive les mouvements
+            // controler.CheckIfRunning();
+            rigidbody.isKinematic = false;
+            player.GetComponent<Animator>().SetTrigger("Default");
+            playerEvent.humanTask = HumanTasks.Nothing;
+            var sword = player.transform.Find(
+                "Root/Hips/Spine_01/Spine_02/Spine_03/Clavicle_R/Shoulder_R/Elbow_R/Hand_R/sword");
+            sword.gameObject.SetActive(false);
+            
+        }
+
         private IEnumerator MovingOn(GameObject player)
         {
-            PlayerControler playerControler = player.GetComponent<PlayerControler>();
-            while(player)
+            var playerEvent = player.GetComponent<PlayerEvent>();
+            while (playerEvent.humanTask == HumanTasks.Zipline)
             {
-                player.transform.Translate(translateVector3.normalized * Time.deltaTime);
+                player.transform.Translate(translateVector3.normalized * Time.deltaTime * speed);
                 yield return new WaitForEndOfFrame();
             }
         }
-        
     }
 }
--- Assets/Scripts/Objects/ZiplineExit.cs
+++ Assets/Scripts/Objects/ZiplineExit.cs
@@ -1,9 +1,31 @@
+using People;
+using People.Player;
 using UnityEngine;
 
 namespace Objects
 {
     public class ZiplineExit : MonoBehaviour
     {
-        
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("MyPlayer")) // si collision avec autre joueur
+            {
+                var playerEvent = other.GetComponent<PlayerEvent>();
+                if (playerEvent.humanTask == HumanTasks.Zipline)
+                {
+                    PlayerControler controler = other.GetComponent<PlayerControler>();
+                    controler.enabled = true;
+                    other.gameObject.GetComponent<Rigidbody>().useGravity = true; // on réactive la gravité du joueur
+                    controler.SetRotateBool(true);
+                    controler.SetMoveBool(true); // on réactive les mouvements
+                    // controler.CheckIfRunning();
+                    other.GetComponent<Rigidbody>().isKinematic = false;
+                    other.GetComponent<Animator>().SetTrigger("Default");
+                    playerEvent.humanTask = HumanTasks.Nothing;
+                    var sword = other.transform.Find("Root/Hips/Spine_01/Spine_02/Spine_03/Clavicle_R/Shoulder_R/Elbow_R/Hand_R/sword");
+                    sword.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
