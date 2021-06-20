aa0f989f60230864d7b2147148b5be8db18c13f1
Dov DEVERS <renaud-dov.devers@epita.fr>
2021-06-20 20:00:50 +0200

    add powers on HUD (#98)

--- Assets/Scripts/Objects/Powers/KnifeThrowing.cs
+++ Assets/Scripts/Objects/Powers/KnifeThrowing.cs
@@ -16,7 +16,8 @@ namespace Objects.Powers
 
         private Transform Right_Hand;
 
-        private void Start()
+
+        protected override void SetValues()
         {
             Right_Hand = gameObject.transform
                 .Find("Root/Hips/Spine_01/Spine_02/Spine_03/Clavicle_L/Shoulder_L/Elbow_L/Hand_L").transform;
@@ -31,11 +32,11 @@ namespace Objects.Powers
             if (!_selectedTarget.IsTarget())
                 return false;
             var distance = Vector3.Distance(transform.position, _selectedTarget.GetTarget().transform.position);
-            
-            if (distance >= _maxDistance || Physics.Raycast(transform.position,transform.forward,distance,0))
+
+            if (distance >= _maxDistance || Physics.Raycast(transform.position, transform.forward, distance, 0))
                 return false;
             var humanEvent = _selectedTarget.GetTarget().GetComponent<HumanEvent>();
-            return humanEvent.humanTask != HumanTasks.Bleeding && humanEvent.humanTask != HumanTasks.SpeedRunning;
+            return humanEvent.humanTask != HumanTasks.Bleeding && humanEvent.humanTask != HumanTasks.SpeedRunning && humanEvent.humanTask != HumanTasks.Zipline;
         }
 
         protected override void Action()
@@ -47,10 +48,10 @@ namespace Objects.Powers
         {
             GetComponent<Animator>().SetTrigger("throw");
             yield return new WaitForSeconds(0.8f);
-            
+
             var target = _selectedTarget.GetTarget();
             target.GetComponent<HumanEvent>().HarmedByKnife();
-            
+
 
             var fxInstance = Instantiate(fx, Right_Hand.position + 0.2f * Vector3.up,
                 Quaternion.LookRotation(Right_Hand.position - target.transform.position));
