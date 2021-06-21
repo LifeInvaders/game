3d943e9d17f839001520c863d194abf6f8e3d86c
Paul de la Porte <paul.de-la-porte-des-vaux@epita.fr>
2021-06-21 19:01:22 +0200

    add menu pause

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -1,6 +1,9 @@
 using System;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UI
@@ -9,41 +12,59 @@ namespace UI
     {
         [SerializeField] private GameObject[] menus;
         [SerializeField] private PlayerInput playerinput;
-        void Update()
-            {
-                if (Input.GetKeyDown(KeyCode.Escape))
-                    GameObject.Find("Back").GetComponent<Button>().onClick.Invoke();
-            }
-        
-            public void SwitchMenu(int index)
-            {
-               
-                for (int i = 0; i < menus.Length; i++)
-                    menus[i].SetActive(i==index);
-            }
+        [SerializeField] private GameObject pauseMenuHUD;
 
-            public void LeaveGame()
+
+        void Back()
+        {
+            var find = GameObject.Find("Back");
+            if (find is null)
             {
-                
-            }
-            
-            public void Resume()
-            {
-                transform.GetChild(0).gameObject.SetActive(false);
-                playerinput.SwitchCurrentActionMap("Player");
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
+                Resume();
+                return;
             }
 
-            public void OnPause()
+            find.GetComponent<Button>().onClick.Invoke();
+        }
+
+        public void SwitchMenu(int index)
+        {
+            for (int i = 0; i < menus.Length; i++)
+                menus[i].SetActive(i == index);
+        }
+
+        public void LeaveGame()
+        {
+            if (PhotonNetwork.IsConnected)
+                PhotonNetwork.LeaveRoom();
+            else
+                SceneManager.LoadScene("Scenes/MainMenu");
+        }
+
+        public void Resume()
+        {
+            pauseMenuHUD.SetActive(false);
+            playerinput.SwitchCurrentActionMap("Player");
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        public void OnPause()
+        {
+            if (pauseMenuHUD.activeSelf)
             {
-                // if (gameObject.activeSelf) return;
-                Debug.Log("cc");
-                transform.GetChild(0).gameObject.SetActive(true);
+                Back();
+            }
+            else
+            {
+                Debug.Log(pauseMenuHUD.activeSelf);
+                pauseMenuHUD.SetActive(true);
                 playerinput.SwitchCurrentActionMap("Menu");
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
-
             }
+        }
     }
 }
