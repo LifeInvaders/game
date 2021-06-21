d118db2c39ef6a346a47d4269025a13aa8050080
ModEnter <73334994+ModEnter@users.noreply.github.com>
2021-06-21 19:14:50 +0200

    Add pause Menu (#101)
    * Auto stash before merge of "main" and "Upstream/dev"
    
    * add powers on HUD
    
    * push
    
    * update pause menu
    
    * update second map
    
    * add menu pause
    
    Co-authored-by: Dov DEVERS <dov@chavers.org>
    Co-authored-by: Dov DEVERS <dov.devers@gmail.com>

---
+++ Assets/Scripts/MainMenu/JoinPrivateRoom.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MainMenu
+{
+    public class JoinPrivateRoom : MonoBehaviour
+    {
+      [SerializeField] private TMP_InputField codeField;
+
+        [Header("Logs")] [SerializeField] private TextMeshProUGUI errorLog;
+        [SerializeField] private GameObject logBlock;
+
+
+        void Start()
+        {
+            logBlock.SetActive(false);
+        }
+
+
+        void ShowError(string message)
+        {
+            logBlock.SetActive(true);
+            errorLog.text = message;
+        }
+
+        public void JoinRoom()
+        {
+            logBlock.SetActive(false);
+            if (codeField.text == "")
+            {
+                ShowError("Code Field can't be Empty");
+                return;
+            }
+
+            if (codeField.text.Contains(" "))
+            {
+                ShowError("No Space Allowed In Code");
+                return;
+            }
+            try
+            {
+                //PhotonNetwork.JoinRoom(codeField.text);
+                Debug.Log("Room Joined !!");
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
